bcdf428d15aaec6e12a7bf961d53b9e708ec5eb7
Gabriela Tchian <gabriela.tchian@dextra-sw.com>
2021-06-25 10:56:23 -0300

    segundo dia

---
+++ lib/modules/home/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+    );
+  }
+}
